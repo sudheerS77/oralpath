da57907cfcf02d1b06dcc7350441f690add41faa
gsksudheer <sudheer.gsk77@gmail.com>
2022-09-26 18:27:46 +0530

    first commit

--- client/src/key.js
+++ client/src/key.js
@@ -1,8 +1,9 @@
-export const API_URL = window.location.hostname.includes("localhost")
-  ? "http://localhost:4000"
-  : "/";
+export const API_URL = "http://localhost:4000/opapi";
+// window.location.hostname.includes("localhost")
+// ? "http://localhost:4000"
+// : "/node";
 
-export const CLIENT_URL = window.location.hostname.includes("localhost")
-  ? "http://localhost:3000"
-  : "/";
-
+export const CLIENT_URL = "http://localhost:3000";
+// window.location.hostname.includes("localhost")
+// ? "http://localhost:3000"
+// : "/";
